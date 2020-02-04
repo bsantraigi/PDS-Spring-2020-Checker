c88a6e88d15999206858f279b1451a6835aae5bf
Bishal Santra <bsantraigi@gmail.com>
2020-02-04 17:38:00 +0530

    Add files via upload

--- find_students.py
+++ find_students.py
@@ -4,8 +4,8 @@ import csv
 from subprocess import STDOUT, check_output
 import ntpath
 
-folder = 'Assignment 2_a_/'
-custom_input = b"56 58 1 33\n"
+folder = 'Assignment 1_e_/'
+custom_input = b"0 4.0 3 0 1 3\n"
 # _required_files = ['encode.txt', 'log.txt', 'decode.txt']
 _required_files = []
 required_files = [os.path.join(folder, f) for f in _required_files]
@@ -45,8 +45,8 @@ for i, fx in enumerate(flist):
 
 			# print(old_path)
 			# print(new_folder)
-			print(mkdir_cmd)
-			print(mv_cmd)
+			# print(mkdir_cmd)
+			# print(mv_cmd)
 			os.system(mkdir_cmd)
 			os.system(mv_cmd)
 			for eff in required_files:
@@ -82,6 +82,8 @@ def dict_to_csv(D, fname):
 
 f_devnull = open(os.devnull,"w")
 f_md = open(f'{folder.replace("/", "")}.md'.replace(" ", ""), "w")
+f_md.write("## INPUT \n")
+f_md.write(f"`{custom_input.decode(u'utf-8')}`\n")
 marks_all = []
 for fx in flist:
 	print()
@@ -92,18 +94,12 @@ for fx in flist:
 	if os.path.isdir(roll_folder):
 		result = {}
 		result['Roll'] = fx
-		# flag = input(f'Check {fx}? [y]yes,s(skip):')
-		# if flag=='s':
-		# 	continue
+
 		for src_fx in glob.glob(roll_folder + '/*.c'):
-			#src_path = os.path.join(roll_folder, src_fx)
 			src_path = src_fx
 			print(f'SRC: {src_path}')
-			with open(src_path) as src_code:
-				f_md.write(f"### {src_path}\n")
-				f_md.write("```C++\n")
-				f_md.write(src_code.read())
-				f_md.write("\n```\n")
+
+
 			# exe_path = re.sub(r'[ ]+', '_', exe_path)
 			Px = re.search(r"(.+)\.c$", src_path).group(1)
 			_exe_path = re.search(r".+\.c(pp)?$", src_path, re.IGNORECASE).group()
@@ -120,63 +116,35 @@ for fx in flist:
 			if err_code != 0:
 				result[f'{Px}.compile'] = 'Failed'
 				print('FAILED')
+				f_md.write(f"Compilation Status: FAILED\n")
 			else:
 				result[f'{Px}.compile'] = 'Success'
 				print('SUCCESS')
+				f_md.write(f"\n> **Compilation Status**: SUCCESS\n\n")
 
-				# p = subprocess.Popen(shlex.split(f"./{exe_path}"), stdout=sys.stdout, stderr=f_devnull)
-				# run_err_code = p.wait()
-				# if run_err_code == 0:
-				# 	result[f'{Px}.run'] = 'Success'
-				# else:
-				# 	result[f'{Px}.run'] = 'Failed'
-
-				# wd = os.getcwd()
-				# os.chdir(roll_folder)
-
-				# try:
-				# 	output = check_output([f"./{ntpath.basename(l_exe_path)}"], stderr=STDOUT, timeout=10, cwd=roll_folder)
-				# 	result[f'{Px}.run'] = 'Success'
-				# 	# print(output.decode())
-				# except subprocess.CalledProcessError as e:
-				# 	if e.returncode == -11:
-				# 		result[f'{Px}.run'] = 'Segfault'
-				# 	else:
-				# 		result[f'{Px}.run'] = 'Failed'
-				# 	print(e)
-				# except subprocess.TimeoutExpired as e:
-				# 	print(e)
-				# 	result[f'{Px}.run'] = 'Inf.Loop'
-				# except UnicodeDecodeError as e:
-				# 	print(e)
-
-				p = subprocess.run([f'./{l_exe_path}'], stdout=subprocess.PIPE, input=custom_input)
-				if p.stdout is not None:
+				p = subprocess.run([f'./{l_exe_path}'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, input=custom_input)
+				if p.returncode == 0:
+					output = p.stdout.decode(u'utf-8')
 					print("\nOUTPUT:")
-					print(p.stdout.decode(u'utf-8'))
-				if p.stderr is not None:
+					print(output)
+					f_md.write(f"OUTPUT\n---\n```\n{output}\n```\n")
+				else:
 					print("\nERROR:")
-					print(p.stderr.decode(u'utf-8'))
-				# while(1):
-				# 	p = subprocess.Popen(['xterm', '-hold', '-e', f'"./{l_exe_path}"'], stdin=subprocess.PIPE)
-					# if custom_input is not None:
-					# 	p.stdin.write(custom_input)
-					# 	p.communicate()
-					# flag = input('Repeat Test y,[n]:')
-					# if flag != 'y':
-					# 	break
-				
-				# p.wait()
-				# p = subprocess.Popen(shlex.split(f"./{exe_path}"), stdout=sys.stdout, stderr=sys.stderr, shell=True)
-				# err_code = p.wait()
-				# print(f'Enter Marks: {exe_path}')
-				# score = input()
-				# marks_all.append(f"{fx},{exe_path},{int(score)}")
-		
+					err = p.stderr.decode(u'utf-8')
+					print(err)
+					f_md.write(f"ERROR\n---\n```\n{err}\n```\n")
+
+			with open(src_path) as src_code:
+				f_md.write(f"> **SRC:** {src_path}\n")
+				# f_md.write("<details>")
+				# f_md.write("<summary>Source code</summary>")
+				f_md.write("```C++\n")
+				f_md.write(src_code.read())
+				f_md.write("\n```\n")
+				# f_md.write("</details>")
+
 		results[fx] = result
-		# flag = input('Continue? [y]yes,e(exit):')
-		# if flag=='e':
-		# 	break
+
 f_devnull.close()
 f_md.close()
 dict_to_csv(results, 'A5.csv')
